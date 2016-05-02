461f658ef21d309a05b738c887ac9c45c09364a9
taurni <Bug*ep6>
2016-05-02 14:52:47 +0300

    basic adapder WIP

--- adapter.js
+++ adapter.js
@@ -1,11 +1,12 @@
 'use strict';
 const Marko = require('marko');
 module.exports = function(source, config){
-
     let views = null;
 
     function loadViews(source) {
+        // console.log("MARKO:loadView",source);
         views = {};
+
         for (let item of source.flattenDeep()) {
             views['@' + item.handle] = item.content;
             if (item.alias) {
@@ -14,16 +15,28 @@ module.exports = function(source, config){
         }
     }
 
-    source.on('loaded', loadViews);
-    source.on('changed', loadViews);
+    source.on('loaded', function(){
+        // console.log('M:loaded')
+        loadViews
+    });
+    source.on('changed', function(){
+        // console.log('M:changed')
+        loadViews
+    });
 
     return {
         engine: Marko,
         render: function(path, str, context, meta){
-            if (!views === null) loadViews(source);
-            debugger;
+            // console.log("MARKO:renderer","\npath:",path,'\nstr:', str, '\ncontext:', context,'\nmeta:', meta);
+            /* path - readed .arko template file path
+             * str - .marko template file content
+             * context - data
+             * meta - ?undefined?
+             */
+            if (!views) loadViews(source);
+
             //return Promise.resolve(Mustache.render(str, context, views));
-            return Promise.resolve();
+            return Promise.resolve(Marko.load(path, str).renderSync(context));
         }
     }
 
